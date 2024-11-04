8b5da8da8544682c6e09ab448dd6ddafa46d3a15
pahadiniraj <sharma12345niraj@gmail.com>
2024-11-04 10:28:23 +0545

    practice day 1

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-const clickToReslove = new Promise((resolve, reject) => {
-  const button = document.getElementById("btn");
-  const button2 = document.getElementById("btn1");
+// let count = 0;
+// let btn = document.getElementById("state");
+// let btn1 = document.getElementById("state1");
 
-  button.addEventListener("click", () => {
-    resolve("Resolve");
-  });
+// const stateChange = () => {
+//   document.getElementById("counter").innerText = `Counter ${count}`;
+// };
 
-  button2.addEventListener("click", () => {
-    reject("Reject");
-  });
-});
+// const increaseCounter = () => {
+//   count++;
+//   stateChange();
+//   console.log(count);
+// };
 
-clickToReslove
-  .then((resolve) => {
-    console.log(resolve);
-  })
-  .catch((err) => console.log(err));
+// const decreaseCount = () => {
+//   count--;
+//   stateChange();
+//   console.log(count);
+// };
+
+// btn.addEventListener("click", () => {
+//   increaseCounter();
+// });
+
+// btn1.addEventListener("click", () => {
+//   decreaseCount();
+// });
+
+// console.log(count);
+
+// Write a function multiplyNumbers(a, b) that takes two numbers as arguments, multiplies them, and returns the result.
+
+// const multiplyNumbers = (a, b) => {
+//   return a * b;
+// };
+
+// console.log(multiplyNumbers(1, 4));
+
+// Write a function isEven(number) that takes a number as an argument and returns true if the number is even, and false otherwise.
+
+// const isEven = (number) => {
+//   if (number % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// console.log(isEven(12));
+
+// Write a function printNumbers(n) that takes a positive integer n and prints all numbers from 1 to n in the console.
+
+// const printNumber = (n) => {
+//   const number = [];
+
+//   for (let i = 0; i <= n; i++) {
+//     console.log(i);
+//     number[i] = i;
+//   }
+//   console.log(number.length);
+//   return number;
+// };
+
+// console.log(printNumber(10));
